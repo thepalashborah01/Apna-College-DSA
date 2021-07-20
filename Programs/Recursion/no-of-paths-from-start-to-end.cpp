ed8ec9b91a0de4ea152b6c449cf57c0b5c4e2942
Palash Protim Borah <palashborah04@gmail.com>
2021-07-20 10:08:58 +0530

    added 3 recursion programs and notes 16.4

---
+++ Programs/Recursion/no-of-paths-from-start-to-end.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countPath(int start, int end)
+{
+    if(start ==  end)
+        return 1;
+    if(start > end)
+        return 0;
+    int count = 0;
+    for(int i=1; i<=6; i++)
+    {
+        count += countPath(start+i, end);
+    }
+    return count;
+}
+int main()
+{
+    cout<<countPath(0, 3);
+    return 0;
+}
