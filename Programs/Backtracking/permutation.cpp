a1d875f54060b05b9bb8faa23cf650b14ec4c93c
Palash Protim Borah <palashborah04@gmail.com>
2021-07-23 22:49:31 +0530

    last edit : 23.07.21

--- Programs/Backtracking/permutation.cpp
+++ Programs/Backtracking/permutation.cpp
@@ -1,8 +1,29 @@
+//STL trick
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<vector<int>> ans;
 int main()
 {
+    int n; cin>>n;
+    vector<int> arr(n);
+    for(auto &i : arr)
+    {
+        cin>>i;
+    }
+
+    sort(arr.begin(), arr.end());
+
+    do{
+        ans.push_back(arr);
+    }while(next_permutation(arr.begin(), arr.end()));
+
+    for(auto v : ans)
+    {
+        for(auto i : v)
+            cout<<i<<" ";
+            cout<<"\n";
+    }
 
     return 0;
 }
